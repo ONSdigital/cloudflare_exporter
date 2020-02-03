500e387192331464714f3889546f6e945d0ed2ca
Craig Furman <cfurman@gitlab.com>
2020-02-03 11:34:38 +0000

    Split scraping CF from being scraped
    The Cloudflare analytics API exposes metrics in buckets of time, and
    does not expose actual totals. In order to report accurate counters to
    prometheus, we must update metrics on a timer loop, and expose them when
    scraped.
    
    Note that absolute values of these counters cannot be trusted as zero
    is arbitrary.

--- cloudflare_exporter.go
+++ cloudflare_exporter.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/oklog/run"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
@@ -30,16 +32,18 @@ var (
 			Envar("CLOUDFLARE_API_BASE_URL").Default("https://api.cloudflare.com/client/v4").String()
 	cfAnalyticsAPIBaseURL = kingpin.Flag("cloudflare-analytics-api-base-url", "Cloudflare analytics (graphql) API base URL").
 				Envar("CLOUDFLARE_ANALYTICS_API_BASE_URL").Default("https://api.cloudflare.com/client/v4/graphql").String()
+	cfScrapeIntervalSeconds = kingpin.Flag("cloudflare-scrape-interval-seconds", "Interval at which to retrieve metrics from Cloudflare, separate from being scraped by prometheus").
+				Envar("CLOUDFLARE_SCRAPE_INTERVAL_MINUTES").Default("300").Int()
 	scrapeTimeoutSeconds = kingpin.Flag("scrape-timeout-seconds", "scrape timeout seconds").
 				Envar("CLOUDFLARE_EXPORTER_SCRAPE_TIMEOUT_SECONDS").Default("30").Int()
 
-	// metric descriptions
-	zoneCount    = prometheus.NewDesc(prometheus.BuildFQName(namespace, "zones", "count"), "Number of zones in the target Cloudflare account", nil, nil)
-	httpRequests = prometheus.NewDesc(prometheus.BuildFQName(namespace, "zones", "http_requests"), "Number of zones in the target Cloudflare account", nil, nil)
+	// zone metrics
+	zoneCount = prometheus.NewGauge(prometheus.GaugeOpts{Namespace: namespace, Subsystem: "zones", Name: "count", Help: "Number of zones in the target Cloudflare account"})
 
-	// direct instrumentation counters
-	totalScrapes   = prometheus.NewCounter(prometheus.CounterOpts{Namespace: namespace, Subsystem: "exporter", Name: "total_scrapes", Help: "Number of times this exporter has been scraped"})
-	scrapeFailures = prometheus.NewCounter(prometheus.CounterOpts{Namespace: namespace, Subsystem: "exporter", Name: "scrape_failures", Help: "Number of times this exporter has errored on scrape"})
+	// exporter metrics
+	totalScrapes = prometheus.NewCounter(prometheus.CounterOpts{Namespace: namespace, Subsystem: "exporter", Name: "total_scrapes", Help: "Number of times this exporter has been scraped"})
+	cfScrapes    = prometheus.NewCounter(prometheus.CounterOpts{Namespace: namespace, Subsystem: "exporter", Name: "cloudflare_scrapes", Help: "Number of times this exporter has scraped cloudflare"})
+	cfScrapeErrs = prometheus.NewCounter(prometheus.CounterOpts{Namespace: namespace, Subsystem: "exporter", Name: "cloudflare_scrape_errors", Help: "Number of times this exporter has failed to scrape cloudflare"})
 )
 
 func main() {
@@ -53,6 +57,7 @@ func main() {
 		email: *cfEmail, apiKey: *cfAPIKey, apiBaseURL: *cfAPIBaseURL,
 		analyticsAPIBaseURL: *cfAnalyticsAPIBaseURL,
 		scrapeTimeout:       time.Duration(*scrapeTimeoutSeconds) * time.Second,
+		scrapeInterval:      time.Duration(*cfScrapeIntervalSeconds) * time.Second,
 	}
 	prometheus.MustRegister(cfExporter)
 
@@ -64,8 +69,29 @@ func main() {
 	router := http.NewServeMux()
 	router.Handle("/metrics", promhttp.Handler())
 
+	runGroup := run.Group{}
+
 	logger.Printf("listening on %s\n", *listenAddress)
-	logger.Fatal(http.ListenAndServe(*listenAddress, router))
+	serverSocket, err := net.Listen("tcp", *listenAddress)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	runGroup.Add(func() error {
+		return http.Serve(serverSocket, router)
+	}, func(error) {
+		serverSocket.Close()
+	})
+
+	cfScrapeCtx, cancelCfScrape := context.WithCancel(context.Background())
+	runGroup.Add(func() error {
+		return cfExporter.scrapeCloudflare(cfScrapeCtx, logger)
+	}, func(error) {
+		cancelCfScrape()
+	})
+
+	if err := runGroup.Run(); err != nil {
+		logger.Fatal(err)
+	}
 }
 
 type exporter struct {
@@ -73,75 +99,85 @@ type exporter struct {
 	apiKey              string
 	apiBaseURL          string
 	analyticsAPIBaseURL string
+	scrapeInterval      time.Duration
 	scrapeTimeout       time.Duration
 }
 
-func (e *exporter) Describe(descs chan<- *prometheus.Desc) {
-	descs <- zoneCount
-	descs <- totalScrapes.Desc()
-	descs <- scrapeFailures.Desc()
+func (e *exporter) scrapeCloudflare(ctx context.Context, logger *log.Logger) error {
+	ticker := time.Tick(e.scrapeInterval)
+	for {
+		select {
+		case <-ticker:
+			if err := e.scrapeCloudflareOnce(ctx, logger); err != nil {
+				// Returning an error here would cause the exporter to crash. If it
+				// crashloops but prometheus manages to scrape it in between crashes, we
+				// might never notice that we are not updating our cached metrics.
+				// Instead, we should alert on the exporter_cloudflare_scrape_errors
+				// metric.
+				logger.Println(err)
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
 }
 
-func (e *exporter) Collect(metrics chan<- prometheus.Metric) {
-	ctx, cancel := context.WithTimeout(context.Background(), e.scrapeTimeout)
-	defer cancel()
-
-	// TODO does this need to be mutex-ed?
-	totalScrapes.Inc()
-	metrics <- totalScrapes
-
-	e.collectZones(ctx, metrics)
-
-}
-
-func (e *exporter) collectZones(ctx context.Context, metrics chan<- prometheus.Metric) {
-	var err error
+func (e *exporter) scrapeCloudflareOnce(ctx context.Context, logger *log.Logger) (err error) {
 	defer func() {
 		if err != nil {
-			metrics <- prometheus.NewInvalidMetric(zoneCount, err)
-			scrapeFailures.Inc()
-			metrics <- scrapeFailures
+			cfScrapeErrs.Inc()
 		}
 	}()
+
+	logger.Println("scraping Cloudflare")
+	cfScrapes.Inc()
+
+	ctx, cancel := context.WithTimeout(ctx, e.scrapeTimeout)
+	defer cancel()
 
 	// TODO handle >50 zones (the API maximum per page) by requesting successive
 	// pages. For now, we don't anticipate having >50 zones any time soon.
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, e.apiBaseURL+"/zones?per_page=50", nil)
 	if err != nil {
-		return
+		return err
 	}
 	req.Header.Set("X-AUTH-EMAIL", e.email)
 	req.Header.Set("X-AUTH-KEY", e.apiKey)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return
+		return err
 	}
 	if resp.StatusCode != http.StatusOK {
 		err = fmt.Errorf("expected status 200, got %d", resp.StatusCode)
-		return
+		return err
 	}
 
 	defer resp.Body.Close()
 	zones, err := parseZoneIDs(resp.Body)
 	if err != nil {
-		return
+		return err
 	}
-	metrics <- prometheus.MustNewConstMetric(zoneCount, prometheus.GaugeValue, float64(len(zones)))
 
-	e.collectZoneAnalytics(ctx, metrics, zones)
+	zoneCount.Set(float64(len(zones)))
+	return nil
 }
 
-func (e *exporter) collectZoneAnalytics(ctx context.Context, metrics chan<- prometheus.Metric, zones map[string]string) {
-	var err error
-	defer func() {
-		if err != nil {
-			// TODO pick new arbitrary metric
-			// metrics <- prometheus.NewInvalidMetric(zoneCount, err)
-			scrapeFailures.Inc()
-			metrics <- scrapeFailures
-		}
-	}()
+func (e *exporter) Describe(descs chan<- *prometheus.Desc) {
+	descs <- zoneCount.Desc()
+	descs <- totalScrapes.Desc()
+	descs <- cfScrapes.Desc()
+	descs <- cfScrapeErrs.Desc()
+}
+
+func (e *exporter) Collect(metrics chan<- prometheus.Metric) {
+	// TODO does this need to be mutex-ed?
+	totalScrapes.Inc()
+	metrics <- totalScrapes
+
+	metrics <- zoneCount
+	metrics <- cfScrapes
+	metrics <- cfScrapeErrs
 }
 
 func parseZoneIDs(apiRespBody io.Reader) (map[string]string, error) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gitlab.com/gitlab-org/cloudflare_exporter
 go 1.13
 
 require (
+	github.com/oklog/run v1.1.0
 	github.com/prometheus/client_golang v1.4.0
 	github.com/prometheus/common v0.9.1
 	github.com/stretchr/testify v1.4.0
