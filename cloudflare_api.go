129574bc343e05f22fd173d758cb5d997c00e565
Craig Furman <cfurman@gitlab.com>
2020-02-07 12:39:19 +0000

    Split graphql requests into time buckets
    Re-aggregate in order to send prometheus metrics. This enables us to
    track the last time bucket counted for each sum, avoiding double
    counting in all but the first loop.
    
    This also enables us to use pagination when >10000 records are returned
    for different queries.

--- cloudflare_api.go
+++ cloudflare_api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"time"
 )
 
 func parseZoneIDs(apiRespBody io.Reader) (map[string]string, error) {
@@ -19,22 +20,51 @@ func parseZoneIDs(apiRespBody io.Reader) (map[string]string, error) {
 	return zones, nil
 }
 
+func extractZoneHTTPRequests(zoneCounts []httpRequests1mGroupsResp, lastDateTimeCounted time.Time) (map[string]*countryRequestData, time.Time, error) {
+	countries := map[string]*countryRequestData{}
+	var bucketTime time.Time
+	for _, timeBucket := range zoneCounts {
+		var err error
+		bucketTime, err = time.Parse(time.RFC3339, timeBucket.Dimensions.Datetime)
+		if err != nil {
+			return nil, time.Time{}, err
+		}
+
+		if bucketTime.After(lastDateTimeCounted) {
+			for _, countryData := range timeBucket.Sum.CountryMap {
+				if _, ok := countries[countryData.ClientCountryName]; !ok {
+					countries[countryData.ClientCountryName] = &countryRequestData{}
+				}
+				countries[countryData.ClientCountryName].requests += countryData.Requests
+				countries[countryData.ClientCountryName].threats += countryData.Threats
+				countries[countryData.ClientCountryName].bytes += countryData.Bytes
+			}
+		}
+	}
+	return countries, bucketTime, nil
+}
+
 type httpRequestsResp struct {
 	Viewer struct {
 		Zones []struct {
-			ReqGroups []struct {
-				Sum struct {
-					CountryMap []struct {
-						ClientCountryName string `json:"clientCountryName"`
-						Requests          uint64 `json:"requests"`
-						Threats           uint64 `json:"threats"`
-						Bytes             uint64 `json:"bytes"`
-					} `json:"countryMap"`
-				} `json:"sum"`
-			} `json:"httpRequests1mGroups"`
-			ZoneTag string `json:"zoneTag"`
+			ReqGroups []httpRequests1mGroupsResp `json:"httpRequests1mGroups"`
+			ZoneTag   string                     `json:"zoneTag"`
 		} `json:"zones"`
 	} `json:"viewer"`
+}
+
+type httpRequests1mGroupsResp struct {
+	Dimensions struct {
+		Datetime string `json:"datetime"`
+	} `json:"dimensions"`
+	Sum struct {
+		CountryMap []struct {
+			ClientCountryName string `json:"clientCountryName"`
+			Requests          uint64 `json:"requests"`
+			Threats           uint64 `json:"threats"`
+			Bytes             uint64 `json:"bytes"`
+		} `json:"countryMap"`
+	} `json:"sum"`
 }
 
 type zonesResp struct {
@@ -43,4 +73,10 @@ type zonesResp struct {
 		Name   string `json:"name"`
 		Status string `json:"status"`
 	} `json:"result"`
+}
+
+type countryRequestData struct {
+	requests uint64
+	threats  uint64
+	bytes    uint64
 }
--- cloudflare_exporter_test.go
+++ cloudflare_exporter_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -15,4 +17,48 @@ func TestParseZoneIDs_ReturnsMapOfNonPendingZones(t *testing.T) {
 	zones, err := parseZoneIDs(f)
 	require.Nil(t, err)
 	assert.Equal(t, zones, map[string]string{"zone-1-id": "zone-1", "zone-2-id": "zone-2"})
+}
+
+func TestExtractZoneHTTPRequests(t *testing.T) {
+	for _, testCase := range []struct {
+		name                string
+		lastDateTimeCounted string
+		expectedData        map[string]*countryRequestData
+	}{
+		{
+			name:                "sums data by country for buckets later than specified time",
+			lastDateTimeCounted: "2020-02-06T10:01:00Z",
+			expectedData: map[string]*countryRequestData{
+				"CZ": &countryRequestData{requests: 3, threats: 0, bytes: 400},
+				"DE": &countryRequestData{requests: 3, threats: 1, bytes: 400},
+				"GB": &countryRequestData{requests: 24, threats: 0, bytes: 200},
+			},
+		},
+		{
+			name:                "sums data by country for all buckets when the specified time is before them all",
+			lastDateTimeCounted: "1970-01-01T00:00:00Z",
+			expectedData: map[string]*countryRequestData{
+				"CZ": &countryRequestData{requests: 4, threats: 0, bytes: 500},
+				"DE": &countryRequestData{requests: 27, threats: 1, bytes: 600},
+				"GB": &countryRequestData{requests: 24, threats: 0, bytes: 200},
+			},
+		},
+	} {
+		t.Run(testCase.name, (func(t *testing.T) {
+			testDataFile, err := os.Open("testdata/http_reqs_by_country_resp.json")
+			require.Nil(t, err)
+			defer testDataFile.Close()
+
+			var gqlResp map[string]httpRequestsResp
+			require.Nil(t, json.NewDecoder(testDataFile).Decode(&gqlResp))
+
+			lastDateTimeCounted, err := time.Parse(time.RFC3339, testCase.lastDateTimeCounted)
+			require.Nil(t, err)
+
+			countries, newLastDateTime, err := extractZoneHTTPRequests(gqlResp["data"].Viewer.Zones[0].ReqGroups, lastDateTimeCounted)
+			require.Nil(t, err)
+			assert.Equal(t, countries, testCase.expectedData)
+			assert.Equal(t, newLastDateTime, time.Date(2020, time.February, 6, 10, 3, 0, 0, time.UTC))
+		}))
+	}
 }
